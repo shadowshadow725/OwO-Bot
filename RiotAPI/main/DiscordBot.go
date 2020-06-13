51dbb3ffc8e8e354ecce2126d92ad07a4d7d086f
houxinha <xinhao.hou@mail.utoronto.ca>
2020-06-13 17:33:11 -0400

    added function to update Riot apikey and help message

--- RiotAPI/main/DiscordBot.go
+++ RiotAPI/main/DiscordBot.go
@@ -10,16 +10,19 @@ import (
 )
 var token string
 var apikey string
+var helpmsg = "!owo search <jungler username> - try to predict enemy jungle\n!owo help - display this help message\n"
+var apikey_updater string // discord user that's allowed to update the api key
+
 var buffer = make([][]byte, 0)
 
 
-func StartBot(discord_token string, key string){
+func StartBot(discord_token string, key string, updater string ){
 
-
+	apikey_updater = updater
 	token = discord_token
 	apikey = key
 	if token == "" {
-		fmt.Println("No token provided. Please run: airhorn -t <bot token>")
+		fmt.Println("No token provided. Exiting")
 		return
 	}
 
@@ -56,9 +59,8 @@ func StartBot(discord_token string, key string){
 // This function will be called (due to AddHandler above) when the bot receives
 // the "ready" event from Discord.
 func ready(s *discordgo.Session, event *discordgo.Ready) {
-
 	// Set the playing status.
-	s.UpdateStatus(0, "the enemy jungler")
+	s.UpdateStatus(0, "!owo help")
 }
 
 // This function will be called (due to AddHandler above) every time a new
@@ -71,19 +73,29 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	// check if the message is "!airhorn"
-	if strings.HasPrefix(m.Content, "!owo") {
 
+	if strings.HasPrefix(m.Content, "!owo") {
+		fmt.Printf("%s called the bot\n", m.Author.ID)
 		// Find the channel that the message came from.
 		_, err := s.State.Channel(m.ChannelID)
 		if err != nil {
 			// Could not find channel.
 			return
 		}
-
-		username := strings.Replace(m.Content, "!owo ", "", 1)
-		result := SearchPlayer(apikey, username, 20)
-		s.ChannelMessageSend(m.ChannelID, result)
-
+		if strings.HasPrefix(m.Content, "!owo search"){
+			username := strings.Replace(m.Content, "!owo search ", "", 1)
+			result := SearchPlayer(apikey, username, 20)
+			s.ChannelMessageSend(m.ChannelID, result)
+		}else if strings.HasPrefix(m.Content, "!owo update"){
+			if m.Author.ID == apikey_updater{
+				apikey = strings.Replace(m.Content, "!owo update ", "", 1)
+				apikey = strings.ReplaceAll(apikey, " ", "")
+				s.ChannelMessageSend(m.ChannelID, "api key update success\n")
+			}
+		}else if strings.HasPrefix(m.Content, "!owo help"){
+			s.ChannelMessageSend(m.ChannelID, helpmsg)
+		}else{
+			s.ChannelMessageSend(m.ChannelID, "IDK what you want me to do\n")
+		}
 	}
 }
--- RiotAPI/main/main.go
+++ RiotAPI/main/main.go
@@ -4,14 +4,14 @@ import "flag"
 
 var key string
 var discord_token string
-
+var updater_id string
 
 func main (){
 	flag.StringVar(&discord_token, "t", "", "Bot Token")
 	flag.StringVar(&key, "k", "", "Riot API key")
-
+	flag.StringVar(&updater_id, "u", "", "updater_id")
 	flag.Parse()
-	StartBot(discord_token, key)
+	StartBot(discord_token, key, updater_id)
 
 	//imgtest()
 	//SearchPlayer(, "DISCORD TARZANED", 20)
